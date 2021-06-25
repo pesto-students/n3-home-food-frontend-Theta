555cc8ffb8c7f247a6a6b6167a327da3c873ed41
sahil thakare <sahilthakare1998@gmail.com>
2021-06-25 18:52:01 +0530

    base urls setup

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,3 @@
-export const baseUrl = 'http://localhost:4000/api/v1'
-
-
-
-// export const baseUrl = 'https://pesto-home-food-backend.herokuapp.com/api/v1'
-// export const baseUrlAdmin = 'https://pesto-home-food-backend.herokuapp.com/api/v1'
-// export const baseUrlSeller= 'https://pesto-home-food-backend.herokuapp.com/api/v1/sellers'
+export const baseUrl = 'https://pesto-home-food-backend.herokuapp.com/api/v1'
+export const baseUrlAdmin = 'https://pesto-home-food-backend.herokuapp.com/api/v1'
+export const baseUrlSeller= 'https://pesto-home-food-backend.herokuapp.com/api/v1/sellers'

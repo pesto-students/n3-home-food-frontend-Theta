12e1e60dedf168fce7c59109fc1c40540c04355f
FIRST_NAME LAST_NAME <kapil.sharma@vconnekt.com>
2021-07-02 01:25:17 +0530

    add basic test case

---
+++ src/components/shared/__test__/image.test.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import renderer from "react-test-renderer";
+import Image from "../image/image";
+
+it("data not found", () => {
+  const tree = renderer
+    .create(<Image height={200} width={200} url="" redius="20px" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
--- src/components/shared/cart/cart.js
+++ src/components/shared/cart/cart.js
@@ -1,6 +1,7 @@
+import React, { useEffect, useState } from "react";
+
 import { Button, Col, Row } from "antd";
 import "antd/dist/antd.css";
-import { React, useEffect, useState } from "react";
 import axios from "../../../utils/axios";
 import { baseUrl, rupeeSign } from "../../../utils/constant";
 import "./cart.css";
@@ -10,22 +11,21 @@ import { sessionId } from "../../../utils/helpers";
 import { withRouter } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
-const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
+const Cart = ({ alreadyInCart, reloadCart, ...props }) => {
   const { t } = useTranslation();
 
   const [isLoading, setIsLoding] = useState(false);
 
   let userId = sessionId();
 
-  useEffect(()=>{
-  },[alreadyInCart])
+  useEffect(() => {}, [alreadyInCart]);
   const addItems = (dish, method) => {
     setIsLoding(true);
     let currentProduct = alreadyInCart.items.filter(
       (item) => item.productId === dish.productId
     );
 
-    console.log('cart', currentProduct[0]);
+    console.log("cart", currentProduct[0]);
 
     let cartItem = {};
     if (currentProduct.length > 0) {
@@ -39,14 +39,14 @@ const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
         productId: dish.productId,
         quantity: currentProduct[0].quantity,
         userId: userId,
-        price:currentProduct[0].price
+        price: currentProduct[0].price,
       };
     } else {
       cartItem = {
         productId: dish.productId,
         quantity: 1,
         userId: userId,
-        price:currentProduct[0].price
+        price: currentProduct[0].price,
       };
     }
 
@@ -54,7 +54,7 @@ const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
   };
 
   const updateCart = (cartItem) => {
-    console.log('cartitem-update',cartItem)
+    console.log("cartitem-update", cartItem);
     axios
       .post(`${baseUrl}/cart`, cartItem)
       .then((result) => {
@@ -87,9 +87,8 @@ const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
   }
 
   const onCheckout = () => {
-    props.history.push('/checkout')
-  }
-  
+    props.history.push("/checkout");
+  };
 
   return (
     <>
@@ -112,7 +111,7 @@ const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
                         loading={isLoading}
                         onClick={() => addItems(dish, "add")}
                       >
-                         {t('Cart.Add')}
+                        {t("Cart.Add")}
                       </Button>
                     ) : (
                       <div>
@@ -140,7 +139,7 @@ const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
 
         <hr></hr>
         <Row justify="space-between" className="sub-total">
-          <span className="bold">{t('Cart.Sub Total')}</span>
+          <span className="bold">{t("Cart.Sub Total")}</span>
           <span className="bold">
             {rupeeSign}
             {alreadyInCart.subTotal}
@@ -149,7 +148,7 @@ const Cart = ({ alreadyInCart, reloadCart , ...props }) => {
 
         <Row className="checkout">
           <Button type="primary" block onClick={onCheckout}>
-             {t('Cart.Checkout')}
+            {t("Cart.Checkout")}
           </Button>
         </Row>
       </Row>
